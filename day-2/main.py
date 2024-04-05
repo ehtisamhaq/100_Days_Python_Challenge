e10d8d8dacace7e54e9c92f10553b2b3762e541d
Eh-haque <ehtisamul@gmail.com>
2024-04-05 16:45:24 +0600

    day 2 complete

---
+++ day-2/main.py
@@ -0,0 +1,5 @@
+yourName= input("What is your name?: ")
+yourAge= input("How old are you?: ")
+yourWork= input("What are you doing now?: ")
+
+print("Hi i am", yourName,"." " I am", yourAge, "years old." " now i am doing", yourWork,".")
