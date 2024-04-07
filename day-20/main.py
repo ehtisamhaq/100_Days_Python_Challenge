75f49122f1ad63fb55346e7c7d5b03e55948fc7c
Eh-haque <ehtisamul@gmail.com>
2024-04-08 03:00:55 +0600

    day 20 complete

---
+++ day-20/main.py
@@ -0,0 +1,18 @@
+# print("Teen times table")
+# for i in range(1, 1002, 25):
+#     print(i, "x 10 = ", i*10)
+
+# for i in range(100, -1, -3):
+#     print(i)
+
+print("Welcome to my number list generator.")
+print()
+print("You are going to give me a number you want to start with, an ending number, and by how many you want me to add each time.")
+print()
+
+x = int(input("What number do you want to start with? "))
+y = int(input("What number do you want to end with? "))
+z = int(input("How many should I add each time? "))
+
+for i in range(x, y, z):
+  print(i)
