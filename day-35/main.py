bab6ae5fe47be2cf88a34f029b07c38ea9d97b41
Eh-haque <ehtisamul@gmail.com>
2024-04-11 21:50:36 +0600

    day 35 complete

---
+++ day-35/main.py
@@ -0,0 +1,36 @@
+import os, time
+
+todoItem = []
+
+def printTodoItem ():
+    print()
+    for index in range(len(todoItem)):
+        print(f"{index}: {todoItem[index]}")
+    time.sleep(1)
+
+
+while True:
+    print("TODO LIST")
+    menu = input("1: Add item\n2: Remove item\n3: Edit item\n4: Delete items\n5: Show items> ")
+
+    if menu == "1":
+        item = input("Item > ").title()
+        todoItem.append(item)
+    elif menu == "2":
+        item = input("Item > ").title()
+        check = input("Are you sure you want to remove this?\n")
+        if check[0]=="y":
+            if item in todoItem:
+                todoItem.remove(item)
+    elif menu == "3":
+        item = input("What do you want to edit?\n").title()
+        new = input("What do you want to change it to?\n").title()
+        for i in range(0,len(todoItem)):
+            if todoItem[i]==item:
+                todoItem[i]=new
+    elif menu == "4":
+       todoItem = []
+    elif menu == "5":
+       printTodoItem()
+    time.sleep(1)
+    os.system("cls")
