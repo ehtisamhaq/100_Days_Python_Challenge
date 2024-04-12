e1cb4481212c603d224a6a69b73ab661a92d3aa3
Eh-haque <ehtisamul@gmail.com>
2024-04-12 20:23:43 +0600

    day 37 complete

---
+++ day-37/main.py
@@ -0,0 +1,4 @@
+string = "Hello my dare friend!"
+print(string[0:15:2]) # start position : end position : jump between 
+print(string[::-1]) # reverse a string
+print(string.split("e")) # reverse a string
