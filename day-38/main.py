3c391e2983e307be487bd332fbc8b62ad60c0ed1
Eh-haque <ehtisamul@gmail.com>
2024-04-12 20:44:58 +0600

    day 38 complete

---
+++ day-38/main.py
@@ -0,0 +1,19 @@
+def colorChange(color):
+  if color=="r":
+    print("\033[31m", end="")
+  elif color==" ":
+    print("\033[0m", end="")
+  elif color=="b":
+    print("\033[34m", end="")
+  elif color=="y":
+    print("\033[33m", end="")
+  elif color == "g":
+    print("\033[32m", end="")
+  elif color == "p":
+    print("\033[35m", end="")
+
+string = "Become one with the rainbow my young rainbows"
+for letter in string:
+    colorChange(letter.lower())
+    print(letter, end="")
+print("\033[0m")
---
+++ day-38/readme.md
@@ -0,0 +1,3 @@
+**Today I have learned:**
+
+- string slicing
