d1c95e13c9006b54009fab5cfcd1cd153d605cb1
Eh-haque <ehtisamul@gmail.com>
2024-04-14 21:24:41 +0600

    day 42 complete

--- day-42/main.py
+++ day-42/main.py
@@ -1,8 +1,23 @@
-website = {"name": None, "url": None, "desc": None, "rating": None}
+pokemon = {"Name": None, "Type": None, "Move": None,}
 
-for name in website.keys():
-  website[name] = input(f"{name}: ")
+print("PokeMon")
 
+for key, value in pokemon.items():
+    pokemon[key] = input(f"{key}:\t").strip().title()
+
+if(pokemon["Type"] == "Earth"):
+    print("\033[32m", end="")
+elif pokemon["Type"] == "Air":
+    print("\033[37m", end="")
+elif pokemon["Type"] == "Fire":
+    print("\033[31m", end="")
+elif pokemon["Type"] == "Water":
+    print("\033[34", end="")
+else:
+    print("\033[33", end="")
+    
 print()
-for name, value in website.items():
-  print(f"{name}: {value}")
+for key, value in pokemon.items():
+    print(f"{key:<15}: {value}")
+
+print("\033[0m", end="")
